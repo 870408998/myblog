566cbc001d986975029bd6e1e3da9e4f8739ef85
高欣 <870408998@qq.com>
2022-11-18 15:21:50 +0800

    start

---
+++ src/.vuepress/.temp/pwa2/config.js
@@ -0,0 +1,14 @@
+import { defineClientConfig } from "@vuepress/client";
+import { setupPWA } from "/Users/gaoxin/Documents/myCode/study/js/2022-nodejs/node/dir/node_modules/vuepress-plugin-pwa2/lib/client/composables/setup.js";
+import SWUpdatePopup from "/Users/gaoxin/Documents/myCode/study/js/2022-nodejs/node/dir/node_modules/vuepress-plugin-pwa2/lib/client/components/SWUpdatePopup.js";
+
+
+export default defineClientConfig({
+  setup: () => {
+    setupPWA();
+  },
+  rootComponents: [
+    SWUpdatePopup,
+    
+  ],
+});
